162c115fa5bf78b778083e5e7e322444023139e3
Scott Kitterman <scott@kitterman.com>
2018-02-17 18:42:19 -0500

    Added untested sysv init file and added warnings to README

--- setup.py
+++ setup.py
@@ -51,7 +51,9 @@ setup(
     include_package_data=True,
     data_files=[(os.path.join('share', 'man', 'man5'),
         ['man/dkimpy-milter.conf.5']), ('etc', ['etc/dkimpy-milter.conf']),
-        (os.path.join('/lib', 'systemd', 'system'), ['system/dkimpy-milter.service'])],
+        (os.path.join('/lib', 'systemd', 'system'),
+        ['system/dkimpy-milter.service']),(os.path.join('/etc', 'init.d'),
+        ['system/dkimpy-milter.init'])],
 
     install_requires = ['dkimpy>=0.7', 'pymilter', 'authres>=1.0.2', 'nacl'],
     zip_safe = False,
