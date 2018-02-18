76f2a34fe4d4aa366ea13a58f4b5d1494e01aaec
Scott Kitterman <scott@kitterman.com>
2018-02-18 00:54:39 -0500

    Add initial draft of dkimpy-milter.8 based on opendkim.8

--- setup.py
+++ setup.py
@@ -50,11 +50,11 @@ setup(
     },
     include_package_data=True,
     data_files=[(os.path.join('share', 'man', 'man5'),
-        ['man/dkimpy-milter.conf.5']), ('etc', ['etc/dkimpy-milter.conf']),
+        ['man/dkimpy-milter.conf.5']), (os.path.join('share', 'man', 'man8'),
+        ['man/dkimpy-milter.8']), ('etc', ['etc/dkimpy-milter.conf']),
         (os.path.join('/lib', 'systemd', 'system'),
         ['system/dkimpy-milter.service']),(os.path.join('/etc', 'init.d'),
         ['system/dkimpy-milter.init'])],
-
-    install_requires = ['dkimpy>=0.7', 'pymilter', 'authres>=1.0.2', 'nacl'],
+    install_requires = ['dkimpy>=0.7', 'pymilter', 'authres>=1.0.2', 'PyNaCl'],
     zip_safe = False,
 )
