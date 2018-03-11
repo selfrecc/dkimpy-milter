1d8c309da93b077f40bc93362a3331080039647f
Scott Kitterman <scott@kitterman.com>
2018-03-10 20:06:21 -0500

    Fix setup.py install locations so they are installed correctly and drop unneeded README changes.

--- setup.py
+++ setup.py
@@ -59,8 +59,8 @@ setup(
     data_files=[(os.path.join('share', 'man', 'man5'),
         ['man/dkimpy-milter.conf.5']), (os.path.join('share', 'man', 'man8'),
         ['man/dkimpy-milter.8']), ('etc', ['etc/dkimpy-milter.conf']),
-        (os.path.join('/lib', 'systemd', 'system'),
-        ['system/dkimpy-milter.service']),(os.path.join('/etc', 'init.d'),
+        (os.path.join('lib', 'systemd', 'system'),
+        ['system/dkimpy-milter.service']),(os.path.join('etc', 'init.d'),
         ['system/dkimpy-milter'])],
     zip_safe = False,
     **kw
